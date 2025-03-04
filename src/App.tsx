1d044dd104dd297d96a89357253e0ad5a53272ab
yogesh646 <blee44921@gmail.com>
2025-03-04 12:30:22 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import FileUploader from './components/FileUploader'
+import FileUploader1 from './components/FileUploader1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <FileUploader/>
+   <FileUploader1/>
   )
 }
 
